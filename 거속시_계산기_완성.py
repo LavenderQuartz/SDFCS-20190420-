6149ac3f8bcc676418bda38de5663eb25ce119cd
LavenderQuartz <48612958+LavenderQuartz@users.noreply.github.com>
2019-05-04 11:34:18 +0900

    Update 거속시_계산기_완성.py

--- 거속시_계산기_완성.py
+++ 거속시_계산기_완성.py
@@ -1,30 +1,33 @@
 i=0
 while i<1:
-    print('S = ',end="")
-    S=input()
-    print('V = ',end="")
-    V=input()
-    print('T = ',end="")
-    T=input()
-    if(S==''):
-        if(V==''):
-            print('Please input correct number or blank.')
+    try:
+        print('S = ',end="")
+        S=input()
+        print('V = ',end="")
+        V=input()
+        print('T = ',end="")
+        T=input()
+        if(S==''):
+            if(V==''):
+                print('Please input correct number or blank.')
+            elif(T==''):
+                print('Please input correct number or blank.')
+            else:
+                x=int(V)*int(T)
+                print('S = '+str(x))
+                i=i+1
+        elif(V==''):
+            if(T==''):
+                print('Please input correct number or blank.')
+            else:
+                x=int(S)/int(T)
+                print('V = '+str(x))
+                i=i+1
         elif(T==''):
-            print('Please input correct number or blank.')
-        else:
-            x=int(V)*int(T)
-            print('S = '+str(x))
+            x=int(S)/int(V)
+            print('T = '+str(x))
             i=i+1
-    elif(V==''):
-        if(T==''):
-            print('Please input correct number or blank.')
         else:
-            x=int(S)/int(T)
-            print('V = '+str(x))
-            i=i+1
-    elif(T==''):
-        x=int(S)/int(V)
-        print('T = '+str(x))
-        i=i+1
-    else:
+            print('Please input correct number or blank.')
+    except ValueError:
         print('Please input correct number or blank.')
